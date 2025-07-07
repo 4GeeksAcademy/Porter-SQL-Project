335487a196bc924a81ebd9d33eb215041caf7432
Jack Porter <107512444+jporte111@users.noreply.github.com>
2025-07-07 23:25:24 +0000

    SQL Proj. Completed

--- src/sql/queries.sql
+++ src/sql/queries.sql
@@ -22,31 +22,56 @@ SELECT * FROM observations;
 
 
 -- MISSION 1
--- Your query here;
+SELECT * from observations
+LIMIT 10;
 
 -- MISSION 2
--- Your query here;
+SELECT DISTINCT region_id from observations;
 
 
 -- MISSION 3
--- Your query here;
+SELECT COUNT(DISTINCT species_id) as total_species from observations;
 
 
 -- MISSION 4
--- Your query here;
+SELECT * from observations where region_id = 2;
 
 
 -- MISSION 5
--- Your query here;
+SELECT * from observations where observation_date = '1998-08-08';
 
 
 -- MISSION 6
--- Your query here;
+SELECT region_id, COUNT(*) AS total_observations FROM observations GROUP BY region_iORDER BY total_observations DESC;
 
 
 -- MISSION 7
--- Your query here;
+SELECT species_id COUNT (*) AS total_records FROM observations GROUP BY species_id 
+ORDER BY total_records DESC LIMIT 5;
 
 
 -- MISSION 8
--- Your query here;
+SELECT species_id, COUNT(*) as observation_count from observations group by species_id 
+having observation_count < 5;
+
+-- Mission 9
+SELECT observer, COUNT(*) as total from observations group by observer order by total
+DESC;
+
+-- Mission 10
+SELECT regions.name as region_name, COUNT(observations.id) as total_observations
+from observations
+JOIN regions on observations.region_id = regions.id;
+
+-- Mission 11
+SELECT observations.id, species.scientific_name 
+FROM observations
+JOIN species on observations.species_id = species.id;
+
+-- Mission 12
+SELECT regions.name as region, species.scientific_name, COUNT(*) as total
+from observations
+join species on observations.species_id = species.id
+join regions on observations.region_id = regions.id
+group by region, species.scientific_name
+order by region, total DESC;
